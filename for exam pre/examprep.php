2c8d0e395c214f0dd8652b0073a091f5ed939944
Devendra kumar <devendrapal8682@gmail.com>
2022-10-17 18:47:20 +0530

    HTML files are uploaded

---
+++ for exam pre/examprep.php
@@ -0,0 +1,398 @@
+<!-- <iframe src="cd.pdf" width="800" height="400" mar></iframe> -->
+ <!-- <p> <a href="CD.pdf">Compiler Design</a></p><br>
+
+  <div class="subbox">
+  <p> <a href="CD.pdf">Compiler Design</a></p>
+  </div>
+  
+  <div  class="subbox" >
+  <p> <a href="CD.pdf">Web Technology</a></p>
+  </div>
+   
+  <div class="subbox" >
+  <p> <a href="CD.pdf">Computer Networks</a></p>
+  </div>
+
+   
+  <div class="subbox" >
+  <p> <a href="CD.pdf">Oops</a></p>
+  </div>-->
+
+   
+
+
+
+
+
+  <!-- this is another type of -->
+   
+<!--
+  <div class="col-2.5 or">
+    <div  class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;"> Computer Networks</h1>
+     </div>
+    <ol>
+    <li><h1 class="unit"><a href="PDFS/cn.pdf.pdf">Click here to read unit 1</a></h1></li>
+    <li><h1 class="unit"><a href="PDFS/cn.pdf.pdf">Click here to read unit 2</a></h1></li>
+    <li><h1 class="unit"><a href="PDFS/cn.pdf.pdf">Click here to read unit 3</a></h1></li>
+    <li><h1 class="unit"><a href="PDFS/cn.pdf.pdf">Click here to read unit 4</a></h1></li>
+    <li><h1 class="unit"><a href="PDFS/cn.pdf.pdf">Click here to read unit 5</a></h1></li>
+    </ol>
+   <a href="web1.html" target="_blank" style="text-decoration:none; float:right;font-size: 60px;  margin-right:20px;"  >&#x2192</a>
+</div>
+
+
+<div class="col-2.5 or" >
+    <div class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;">Web Technology</h1>
+     </div>
+
+     <ol>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 1</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 2</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 3</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 4</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 5</a></h1>    </li>
+    </ol>
+   <a href="web1.html" target="_blank" style="text-decoration:none; font-size: 60px; float:right; margin-bottom:50px; margin-right:20px;"  >&#x2192</a>
+</div>
+
+
+<div class="col-2.5 or" >
+    <div class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;">Big Data</h1>
+     </div>
+
+     <ol>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 1</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 2</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 3</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 4</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 5</a></h1>    </li>
+    </ol>
+   <a href="web1.html" target="_blank" style="text-decoration:none; font-size: 60px; float:right; margin-bottom:50px; margin-right:20px;"  >&#x2192</a>
+</div>
+
+
+<div class="col-2.5 or" >
+    <div class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;"> Oops</h1>
+     </div>
+     <ol>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 1</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 2</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 3</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 4</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 5</a></h1>    </li>
+    </ol>
+   <a href="web1.html" target="_blank" style="text-decoration:none; font-size: 60px; float:right; margin-bottom:50px; margin-right:20px;"  >&#x2192</a>
+</div>
+
+<div class="col-2.5 or" >
+    <div class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;"> ITCS</h1>
+     </div>
+
+    <ol>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 1</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 2</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 3</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 4</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 5</a></h1>    </li>
+    </ol>
+   <a href="web1.html" target="_blank" style="text-decoration:none; font-size: 60px; float:right; margin-bottom:50px; margin-right:20px;"  >&#x2192</a>
+</div>
+
+<div class="col-2.5 or" >
+    <div class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;">Software Engineering</h1>
+     </div>
+    <ol>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 1</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 2</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 3</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 4</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 5</a></h1>    </li>
+    </ol>
+    
+   <a href="web1.html" target="_blank" style="text-decoration:none; font-size: 60px; float:right; margin-bottom:50px; margin-right:20px;"  >&#x2192</a>
+</div>
+
+
+<div class="col-2.5 or" >
+    <div class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;"> Java </h1>
+     </div>
+
+    <ol>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 1</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 2</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 3</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 4</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 5</a></h1>    </li>
+    </ol>
+   <a href="web1.html" target="_blank" style="text-decoration:none; font-size: 60px; float:right; margin-bottom:50px; margin-right:20px;"  >&#x2192</a>
+</div>
+
+
+<div class="col-2.5 or" >
+    <div class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;"> Python 3</h1>
+     </div>
+
+    <ol>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 1</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 2</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 3</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 4</a></h1></li>
+    <li><h1 class="unit"><a href="CD.pdf">Click here to read unit 5</a></h1>    </li>
+    </ol>
+   <a href="web1.html" target="_blank" style="text-decoration:none; font-size: 60px; float:right; margin-bottom:50px; margin-right:20px;"  >&#x2192</a>
+</div>
+
+             
+
+<div class="col-2.5 or" >
+    <div class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;"> Python 3</h1>
+     </div>
+
+     <li class="nav-item dropdown">
+
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+          data-bs-toggle="dropdown" aria-expanded="false">UNITWISE PDF </a>
+          <ol class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 1</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 2</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 3</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 4</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 5</a></h1></li>
+         </ol>
+        </li>
+
+
+        <li class="nav-item dropdown">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+        data-bs-toggle="dropdown" aria-expanded="false">UNITWISE PPT </a>
+        <ol class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 1</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 2</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 3</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 4</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 5</a></h1></li>
+        </ol>
+        </li>
+
+   <a href="web1.html" target="_blank" style="text-decoration:none; font-size: 60px; float:right; margin-bottom:50px; margin-right:20px;"  >&#x2192</a>
+</div>
+
+
+
+<div class="col-2.5 or" >
+    <div class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;"> Python 3</h1>
+     </div>
+
+     <li class="nav-item dropdown">
+
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+          data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown </a>
+          <ol class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 1</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 2</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 3</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 4</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 5</a></h1></li>
+         </ol>
+        </li>
+
+
+                <li class="nav-item dropdown">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+        data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown </a>
+        <ol class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 1</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 2</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 3</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 4</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 5</a></h1></li>
+        </ol>
+        </li>
+   <a href="web1.html" target="_blank" style="text-decoration:none; font-size: 60px; float:right; margin-bottom:50px; margin-right:20px;"  >&#x2192</a>
+</div>
+
+
+
+
+
+<div class="col-2.5 or" >
+    <div class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;"> Python 3</h1>
+     </div>
+
+     <li class="nav-item dropdown">
+
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+          data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown </a>
+          <ol class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 1</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 2</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 3</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 4</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 5</a></h1></li>
+         </ol>
+        </li>
+
+
+                <li class="nav-item dropdown">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+        data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown </a>
+        <ol class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 1</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 2</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 3</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 4</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 5</a></h1></li>
+        </ol>
+        </li>
+   <a href="web1.html" target="_blank" style="text-decoration:none; font-size: 60px; float:right; margin-bottom:50px; margin-right:20px;"  >&#x2192</a>
+</div>
+
+
+
+
+
+<div class="col-2.5 or" >
+    <div class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;"> Python 3</h1>
+     </div>
+
+     <li class="nav-item dropdown">
+
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+          data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown </a>
+          <ol class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 1</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 2</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 3</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 4</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 5</a></h1></li>
+         </ol>
+        </li>
+
+
+                <li class="nav-item dropdown">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+        data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown </a>
+        <ol class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 1</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 2</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 3</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 4</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 5</a></h1></li>
+        </ol>
+        </li>
+   <a href="web1.html" target="_blank" style="text-decoration:none; font-size: 60px; float:right; margin-bottom:50px; margin-right:20px;"  >&#x2192</a>
+</div>
+
+
+
+
+
+<div class="col-2.5 or" >
+    <div class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;"> Python 3</h1>
+     </div>
+
+     <li class="nav-item dropdown">
+
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+          data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown </a>
+          <ol class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 1</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 2</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 3</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 4</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 5</a></h1></li>
+         </ol>
+        </li>
+
+
+                <li class="nav-item dropdown">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+        data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown </a>
+        <ol class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 1</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 2</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 3</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 4</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 5</a></h1></li>
+        </ol>
+        </li>
+   <a href="web1.html" target="_blank" style="text-decoration:none; font-size: 60px; float:right; margin-bottom:50px; margin-right:20px;"  >&#x2192</a>
+</div>
+
+
+
+<div class="col-2.5 or" >
+    <div class = "head">
+      
+     <h1 style="font-size:20px; margin-top:0px; margin-left: 20px; color:blue;"> Python 3</h1>
+     </div>
+
+     <li class="nav-item dropdown">
+
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+          data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown </a>
+          <ol class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 1</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 2</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 3</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 4</a></h1></li>
+              <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 5</a></h1></li>
+         </ol>
+        </li>
+
+
+                <li class="nav-item dropdown">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+        data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown </a>
+        <ol class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 1</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 2</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 3</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 4</a></h1></li>
+            <li><h1 class="unit"><a class="dropdown-item" href="CD.pdf">unit 5</a></h1></li>
+        </ol>
+        </li>
+   <a href="web1.html" target="_blank" style="text-decoration:none; font-size: 60px; float:right; margin-bottom:50px; margin-right:20px;"  >&#x2192</a>
+</div>
